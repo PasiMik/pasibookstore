f2f96d4b81a5d2ba53941a6cb3cdac62c7bd2f93
Pasi Mikkola <pasimikkola111@gmail.com>
2022-09-01 13:01:56 +0300

    first commit

---
+++ src/main/java/com/example/pasibookstore/PasibookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.pasibookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasibookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasibookstoreApplication.class, args);
+	}
+
+}
