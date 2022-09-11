2722042967f936f3e77693d0f9bacb7f216af4c2
Pasi Mikkola <pasimikkola111@gmail.com>
2022-09-11 17:47:25 +0300

    Adding booklist.html

--- src/main/java/com/example/pasibookstore/web/BookController.java
+++ src/main/java/com/example/pasibookstore/web/BookController.java
@@ -5,20 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.pasibookstore.domain.Book;
+
 import com.example.pasibookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 	@Autowired
-	private BookRepository repository;
+	private BookRepository repository;	
+
 	
-	@GetMapping("/index")
-	public String bookList(Model model) {
+	@GetMapping("/booklist")
+	public String booklist(Model model) {
 		model.addAttribute("books", repository.findAll());
-		return "index";
+		return "booklist";
 	}
-	
+		
 
 
 }
